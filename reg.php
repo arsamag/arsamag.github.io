938fdc02229a458c522dfef7262b482fe38d6382
arsamag <abitmak@gmail.com>
2017-07-08 22:40:54 +0500

    rune

---
+++ reg.php
@@ -0,0 +1,178 @@
+<html>
+<head>
+<meta http-equiv="Content-Language" content="ru">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>;)</title>
+<meta name="author" content="Arsa">
+<link href="font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="âåäàðü2.css" rel="stylesheet" type="text/css">
+<link href="reg.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="container">
+<div id="Layer4" style="position:absolute;text-align:left;left:0px;top:494px;width:998px;height:149px;z-index:40;">
+<div id="RollOver31" style="position:absolute;left:639px;top:17px;width:124px;height:124px;overflow:hidden;z-index:0">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà.png">
+<span><img alt="" src="images/áîëâàíêà.png"></span>
+</a>
+</div>
+<div id="RollOver19" style="position:absolute;left:42px;top:17px;width:142px;height:58px;overflow:hidden;z-index:1">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver23" style="position:absolute;left:42px;top:82px;width:142px;height:58px;overflow:hidden;z-index:2">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver27" style="position:absolute;left:773px;top:17px;width:142px;height:58px;overflow:hidden;z-index:3">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver26" style="position:absolute;left:773px;top:82px;width:142px;height:58px;overflow:hidden;z-index:4">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver28" style="position:absolute;left:342px;top:17px;width:121px;height:123px;overflow:hidden;z-index:5">
+<a href="./obratnaia_sviaz.php">
+<img class="hover" alt="" src="images/áîëâàíêà2.png">
+<span><img alt="" src="images/áîëâàíêà2.png"></span>
+</a>
+</div>
+<div id="RollOver29" style="position:absolute;left:486px;top:17px;width:142px;height:58px;overflow:hidden;z-index:6">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver30" style="position:absolute;left:486px;top:82px;width:142px;height:58px;overflow:hidden;z-index:7">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver24" style="position:absolute;left:200px;top:17px;width:128px;height:123px;overflow:hidden;z-index:8">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà.png">
+<span><img alt="" src="images/áîëâàíêà.png"></span>
+</a>
+</div>
+<div id="wb_TextArt16" style="position:absolute;left:368px;top:40px;width:73px;height:71px;z-index:9;">
+<a href="http://templum-ignis.ru/" target="_blank"><img src="images/img0107.png" id="TextArt16" alt="&#1096;&#1082;&#1086;&#1083;&#1072; &#1052;&#1072;&#1075;&#1080;&#1080;  &#1054;&#1075;&#1085;&#1103;" title="&#1096;&#1082;&#1086;&#1083;&#1072; &#1052;&#1072;&#1075;&#1080;&#1080;  &#1054;&#1075;&#1085;&#1103;" style="width:73px;height:71px;"></a></div>
+<div id="wb_TextArt18" style="position:absolute;left:50px;top:32px;width:121px;height:29px;z-index:10;">
+<a href="./dr_sait.php"><img src="images/img0108.png" id="TextArt18" alt="&#1044;&#1088;&#1091;&#1078;&#1077;&#1089;&#1090;&#1074;&#1077;&#1085;&#1085;&#1099;&#1077;  &#1089;&#1072;&#1081;&#1090;&#1099;" title="&#1044;&#1088;&#1091;&#1078;&#1077;&#1089;&#1090;&#1074;&#1077;&#1085;&#1085;&#1099;&#1077;  &#1089;&#1072;&#1081;&#1090;&#1099;" style="width:121px;height:29px;"></a></div>
+<div id="wb_TextArt19" style="position:absolute;left:53px;top:98px;width:121px;height:29px;z-index:11;">
+<a href="https://vk.com/magi_v_bolshem_gorode"><img src="images/img0109.png" id="TextArt19" alt="&#1084;&#1072;&#1075;&#1080; &#1074; &#1073;&#1086;&#1083;&#1100;&#1096;&#1086;&#1084; &#1075;&#1086;&#1088;&#1086;&#1076;&#1077;" title="&#1084;&#1072;&#1075;&#1080; &#1074; &#1073;&#1086;&#1083;&#1100;&#1096;&#1086;&#1084; &#1075;&#1086;&#1088;&#1086;&#1076;&#1077;" style="width:121px;height:29px;"></a></div>
+<div id="wb_TextArt20" style="position:absolute;left:498px;top:32px;width:121px;height:29px;z-index:12;">
+<a href="http://koodesnik.su" target="_blank"><img src="images/img0110.png" id="TextArt20" alt="&#1082;&#1091;&#1076;&#1077;&#1089;&#1085;&#1080;&#1082;" title="&#1082;&#1091;&#1076;&#1077;&#1089;&#1085;&#1080;&#1082;" style="width:121px;height:29px;"></a></div>
+<div id="wb_TextArt21" style="position:absolute;left:496px;top:95px;width:121px;height:29px;z-index:13;">
+<a href="http://litta-rey.ru" target="_blank"><img src="images/img0111.png" id="TextArt21" alt="&#1041;&#1083;&#1086;&#1075; &#1051;&#1080;&#1090;&#1090;&#1099; &#1056;&#1077;&#1081;" title="&#1041;&#1083;&#1086;&#1075; &#1051;&#1080;&#1090;&#1090;&#1099; &#1056;&#1077;&#1081;" style="width:121px;height:29px;"></a></div>
+<div id="wb_TextArt22" style="position:absolute;left:216px;top:37px;width:95px;height:81px;z-index:14;">
+<a href="https://vk.com/kras_diamantmagic" target="_blank"><img src="images/img0112.png" id="TextArt22" alt="&#1050;&#1056;&#1040;&#1057; &#1074;&#1086;&#1083;&#1096;&#1077;&#1073;&#1085;&#1072;&#1103;  &#1084;&#1072;&#1089;&#1090;&#1077;&#1088;&#1089;&#1082;&#1072;&#1103;" title="&#1050;&#1056;&#1040;&#1057; &#1074;&#1086;&#1083;&#1096;&#1077;&#1073;&#1085;&#1072;&#1103;  &#1084;&#1072;&#1089;&#1090;&#1077;&#1088;&#1089;&#1082;&#1072;&#1103;" style="width:95px;height:81px;"></a></div>
+<div id="wb_TextArt23" style="position:absolute;left:787px;top:32px;width:114px;height:29px;z-index:15;">
+<a href="http://veito-mage.ru" target="_blank"><img src="images/img0113.png" id="TextArt23" alt="&#1057;&#1072;&#1081;&#1090; &#1084;&#1072;&#1075;&#1072; &#1042;&#1077;&#1081;&#1090;&#1086;" title="&#1057;&#1072;&#1081;&#1090; &#1084;&#1072;&#1075;&#1072; &#1042;&#1077;&#1081;&#1090;&#1086;" style="width:114px;height:29px;"></a></div>
+<div id="wb_TextArt24" style="position:absolute;left:665px;top:39px;width:73px;height:79px;z-index:16;">
+<a href="./donat.php" target="_blank"><img src="images/img0114.png" id="TextArt24" alt="&#1087;&#1086;&#1084;&#1086;&#1095;&#1100; &#1087;&#1088;&#1086;&#1077;&#1082;&#1090;&#1091;" title="&#1087;&#1086;&#1084;&#1086;&#1095;&#1100; &#1087;&#1088;&#1086;&#1077;&#1082;&#1090;&#1091;" style="width:73px;height:79px;"></a></div>
+<div id="wb_TextArt26" style="position:absolute;left:785px;top:90px;width:114px;height:43px;z-index:17;">
+<a href="https://www.livemaster.ru/grimoire" target="_blank"><img src="images/img0115.png" id="TextArt26" alt="&#1043;&#1088;&#1080;&#1084;&#1091;&#1072;&#1088;&#1099; &#1086;&#1090; &#1057;&#1074;&#1077;&#1090;&#1083;&#1072;&#1085;&#1099;" title="&#1043;&#1088;&#1080;&#1084;&#1091;&#1072;&#1088;&#1099; &#1086;&#1090; &#1057;&#1074;&#1077;&#1090;&#1083;&#1072;&#1085;&#1099;" style="width:114px;height:43px;"></a></div>
+</div>
+<div id="Layer2" style="position:absolute;text-align:left;left:233px;top:217px;width:549px;height:262px;z-index:41;">
+<div id="wb_Image1" style="position:absolute;left:38px;top:39px;width:445px;height:178px;z-index:39;">
+<img src="images/ðàçðàáîòêà.png" id="Image1" alt=""></div>
+</div>
+</div>
+<div id="Layer1" style="position:absolute;text-align:center;left:0.9%;top:4px;width:98%;height:176px;z-index:42;">
+<div id="Layer1_Container" style="width:980px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+<div id="RollOver1" style="position:absolute;left:430px;top:10px;width:98px;height:96px;overflow:hidden;z-index:18">
+<a href="./obratnaia_sviaz.php">
+<img class="hover" alt="" src="images/áîëâàíêà2.png">
+<span><img alt="" src="images/áîëâàíêà2.png"></span>
+</a>
+</div>
+<div id="RollOver2" style="position:absolute;left:165px;top:10px;width:258px;height:96px;overflow:hidden;z-index:19">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver3" style="position:absolute;left:55px;top:10px;width:98px;height:96px;overflow:hidden;z-index:20">
+<a href="./obratnaia_sviaz.php">
+<img class="hover" alt="" src="images/áîëâàíêà2.png">
+<span><img alt="" src="images/áîëâàíêà2.png"></span>
+</a>
+</div>
+<div id="wb_Image38" style="position:absolute;left:78px;top:26px;width:53px;height:59px;z-index:21;">
+<a href="./index.php"><img src="images/122482-matte-red-and-white-square-icon-animals-animal-dragon1.png" id="Image38" alt=""></a></div>
+<div id="wb_FontAwesomeIcon3" style="position:absolute;left:455px;top:29px;width:54px;height:50px;text-align:center;z-index:22;">
+<a href="./reg.php"><div id="FontAwesomeIcon3"><i class="fa fa-user-secret">&nbsp;</i></div></a></div>
+<div id="wb_TextArt13" style="position:absolute;left:205px;top:29px;width:179px;height:56px;z-index:23;">
+<a href="./vedar.php"><img src="images/img0116.png" id="TextArt13" alt="&#1042;&#1077;&#1076;&#1072;&#1088;&#1100;" title="&#1042;&#1077;&#1076;&#1072;&#1088;&#1100;" style="width:179px;height:56px;"></a></div>
+<marquee direction="left" scrolldelay="90" scrollamount="6" behavior="scroll" loop="0" style="position:absolute;left:9px;top:120px;width:965px;height:34px;z-index:24;" id="Marquee1"><span style="color:#B22222;font-family:'Courier New';font-size:21px;"><strong>Âî èìÿ ïðàâäû, ïîðÿäêà, ñïðàâåäëèâîñòè è ìèðà</strong></span></marquee>
+<div id="RollOver17" style="position:absolute;left:659px;top:10px;width:89px;height:36px;overflow:hidden;z-index:25">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver18" style="position:absolute;left:774px;top:10px;width:89px;height:36px;overflow:hidden;z-index:26">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver20" style="position:absolute;left:774px;top:70px;width:89px;height:36px;overflow:hidden;z-index:27">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà1.png">
+<span><img alt="" src="images/áîëâàíêà1.png"></span>
+</a>
+</div>
+<div id="RollOver21" style="position:absolute;left:656px;top:65px;width:47px;height:47px;overflow:hidden;z-index:28">
+<a href="./obratnaia_sviaz.php">
+<img class="hover" alt="" src="images/áîëâàíêà2.png">
+<span><img alt="" src="images/áîëâàíêà2.png"></span>
+</a>
+</div>
+<div id="RollOver22" style="position:absolute;left:707px;top:65px;width:47px;height:47px;overflow:hidden;z-index:29">
+<a href="./obratnaia_sviaz.php">
+<img class="hover" alt="" src="images/áîëâàíêà2.png">
+<span><img alt="" src="images/áîëâàíêà2.png"></span>
+</a>
+</div>
+<div id="RollOver16" style="position:absolute;left:534px;top:10px;width:102px;height:102px;overflow:hidden;z-index:30">
+<a href="">
+<img class="hover" alt="" src="images/áîëâàíêà.png">
+<span><img alt="" src="images/áîëâàíêà.png"></span>
+</a>
+</div>
+<div id="wb_TextArt14" style="position:absolute;left:667px;top:14px;width:73px;height:28px;z-index:31;">
+<a href="./obratnaia_sviaz.php"><img src="images/img0117.png" id="TextArt14" alt="&#1086;&#1073;&#1088;&#1072;&#1090;&#1085;&#1072;&#1103;  &#1089;&#1074;&#1103;&#1079;&#1100;" title="&#1086;&#1073;&#1088;&#1072;&#1090;&#1085;&#1072;&#1103;  &#1089;&#1074;&#1103;&#1079;&#1100;" style="width:73px;height:28px;"></a></div>
+<div id="wb_TextArt15" style="position:absolute;left:549px;top:26px;width:73px;height:71px;z-index:32;">
+<a href="./planet_taim.php"><img src="images/img0118.png" id="TextArt15" alt="&#1088;&#1072;&#1089;&#1095;&#1077;&#1090; &#1087;&#1083;&#1072;&#1085;&#1077;&#1090;&#1072;&#1088;&#1085;&#1099;&#1093; &#1095;&#1072;&#1089;&#1086;&#1074;" title="&#1088;&#1072;&#1089;&#1095;&#1077;&#1090; &#1087;&#1083;&#1072;&#1085;&#1077;&#1090;&#1072;&#1088;&#1085;&#1099;&#1093; &#1095;&#1072;&#1089;&#1086;&#1074;" style="width:73px;height:71px;"></a></div>
+<div id="wb_TextArt17" style="position:absolute;left:782px;top:13px;width:73px;height:29px;z-index:33;">
+<a href="./vers_darna.php"><img src="images/img0119.png" id="TextArt17" alt="&#1042;&#1077;&#1088;&#1089; &#1044;&#1072;&#1088;&#1085;&#1072;" title="&#1042;&#1077;&#1088;&#1089; &#1044;&#1072;&#1088;&#1085;&#1072;" style="width:73px;height:29px;"></a></div>
+<div id="wb_TextArt25" style="position:absolute;left:782px;top:74px;width:73px;height:29px;z-index:34;">
+<a href="./vers_darna_ras.php"><img src="images/img0120.png" id="TextArt25" alt="&#1042;&#1077;&#1088;&#1089; &#1044;&#1072;&#1088;&#1085;&#1072; &#1088;&#1072;&#1089;&#1096;&#1080;&#1088;&#1077;&#1085;&#1085;&#1072;&#1103;" title="&#1042;&#1077;&#1088;&#1089; &#1044;&#1072;&#1088;&#1085;&#1072; &#1088;&#1072;&#1089;&#1096;&#1080;&#1088;&#1077;&#1085;&#1085;&#1072;&#1103;" style="width:73px;height:29px;"></a></div>
+<div id="wb_FontAwesomeIcon1" style="position:absolute;left:666px;top:69px;width:28px;height:35px;text-align:center;z-index:35;">
+<a href="./glas.php"><div id="FontAwesomeIcon1"><i class="fa fa-eye">&nbsp;</i></div></a></div>
+<div id="wb_FontAwesomeIcon2" style="position:absolute;left:721px;top:70px;width:21px;height:32px;text-align:center;z-index:36;">
+<a href="./kalendar.php"><div id="FontAwesomeIcon2"><i class="fa fa-calendar">&nbsp;</i></div></a></div>
+</div>
+</div>
+<div id="Layer3" style="position:absolute;text-align:center;left:0.1%;top:659px;width:99.7%;height:50px;z-index:43;">
+<div id="Layer3_Container" style="width:997px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+<div id="wb_Text11" style="position:absolute;left:3px;top:13px;width:329px;height:14px;z-index:37;text-align:left;">
+<span style="color:#555555;font-family:'Courier New';font-size:11px;">Copyright © 2017 <a href="https://vk.com/absalikoff">Arsa</a></span></div>
+<div id="wb_Text10" style="position:absolute;left:840px;top:13px;width:133px;height:14px;text-align:right;z-index:38;">
+<span style="color:#666666;font-family:'Courier New';font-size:11px;">Author: <a href="https://vk.com/absalikoff" target="_blank">ARSA</a> </span></div>
+</div>
+</div>
+</body>
+</html>
